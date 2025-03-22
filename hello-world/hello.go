a38d3221a7375e7a384687cb0147840d65bc0221
whzd <whzdxl@gmail.com>
2025-03-22 12:56:09 +0000

    Finalize Hello

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -1,16 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-const englishHelloPrefix = "Hello, "
+const (
+	spanish               = "spanish"
+	french                = "french"
+	portuguese            = "portuguese"
+	englishHelloPrefix    = "Hello, "
+	spanishHelloPrefix    = "Holla, "
+	frenchHelloPrefix     = "Bonjour, "
+	portugueseHelloPrefix = "Olá, "
+)
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch strings.ToLower(language) {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	case portuguese:
+		prefix = portugueseHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
